5ea5f77fd3cc9d271b59ddc072e8f4b34aeaa4ee
akafaisal <faisalayub2002@gmail.com>
2025-04-29 02:07:21 +0530

    just do it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 "use client";
 
+import Script from "next/script";
+
 export default function Home() {
   return (
-    <div className=" flex justify-center gap-6 h-screen items-center">S</div>
+    <>
+      {/* Load Google Ads Script */}
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5787069373355659"
+        crossOrigin="anonymous"
+      />
+
+      {/* Main Content */}
+      <div className="flex justify-center gap-6 h-screen items-center">S</div>
+    </>
   );
 }
