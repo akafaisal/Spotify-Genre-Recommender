5dc644551520f354cab32187f839474bb9fda46a
Faisal A. <faisalayub2002@gmail.com>
2025-08-29 18:15:32 +0530

    Delete src/app/api/fortune directory

--- src/app/api/fortune/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const fortunes = ["C", "B", "6", "G", "Mi"];
-
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-
-  return Response.json({ message: randomFortune });
-}
