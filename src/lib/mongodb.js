f96866c472c45335176eba8bc9ab3a81ef0f2b1e
Faisal A. <faisalayub2002@gmail.com>
2025-09-09 14:16:26 +0530

    Delete src/lib/mongodb.js

--- src/lib/mongodb.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function connectMongo() {
-  await mongoose.connect("mongodb://localhost:27017/feedback");
-  console.log("MongoDB connected");
-}
