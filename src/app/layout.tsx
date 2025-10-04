f4d0e5e81c56548b4a503c0f0446a87f7355a7a6
Faisal A. <faisalayub2002@gmail.com>
2025-10-04 13:13:58 +0530

    Google Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Spotify Mood Selector",
@@ -12,6 +13,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Tag */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-6RK99CB31Q"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6RK99CB31Q');
+          `}
+        </Script>
+      </head>
       <body>
         <div className="bg-gradient-to-r from-purple-900 to-red-500 animate-gradient-x min-h-screen text-white pl-15">
           {children}
@@ -20,4 +36,3 @@ export default function RootLayout({
     </html>
   );
 }
-
