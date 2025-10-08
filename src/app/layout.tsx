931cdb5f4e67f71685e11259520704705d410245
Faisal A. <faisalayub2002@gmail.com>
2025-10-08 13:10:19 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,15 +15,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google Analytics Tag */}
-       <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-TP5LP3NSM2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TP5LP3NSM2');
-</script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TP5LP3NSM2"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TP5LP3NSM2');
+        </script>
       </head>
       <body>
         <div className="bg-gradient-to-r from-purple-900 to-red-500 animate-gradient-x min-h-screen text-white pl-15">
