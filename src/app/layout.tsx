b75da24ef620386da789b1c6c0b60a9f6db5e422
Faisal A. <faisalayub2002@gmail.com>
2025-10-08 15:05:47 +0530

    GTM tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics Tag */}
+        {/* ✅ Google Tag Manager */}
+        <Script id="gtm-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PG88TFXP');
+          `}
+        </Script>
+
+        {/* ✅ Google Analytics (GA4) */}
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-TP5LP3NSM2"
@@ -29,7 +43,18 @@ export default function RootLayout({
           `}
         </Script>
       </head>
+
       <body>
+        {/* ✅ GTM Noscript Fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PG88TFXP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <div className="bg-gradient-to-r from-purple-900 to-red-500 animate-gradient-x min-h-screen text-white pl-15">
           {children}
         </div>
