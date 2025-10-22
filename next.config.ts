68a8f4bb30757bb7c974577af54ea94c46587483
akafaisal <faisalayub2002@gmail.com>
2025-10-22 13:52:33 +0530

    ezoic ad redirect

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["i.scdn.co"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/ads.txt", // request path
+        destination: "https://srv.adstxtmanager.com/19390/genrify.live", // Ezoic URL
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
